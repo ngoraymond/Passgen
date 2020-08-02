bd1db8c6409e4907c9f484af2bae940e238e803f
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2020-08-01 22:25:21 -0700

    added anilist test

--- Anilist recommendation scrape.py
+++ Anilist recommendation scrape.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-testDict = {}
-
 manga = ['Nisekoi', 'Fuufu-Ijou-Koibito-Miman']
 
 url = 'https://anilist.co/manga/105011/Fuufu-Ijou-Koibito-Miman/'
 
+
 info = requests.get(url).text
 
+testDict = {}
 soupify = BeautifulSoup(info, 'html.parser')
 for x in soupify.find_all('div', {'class':'recommendation-card'}):
     goal = x.find('span').find('span')['aria-label']
